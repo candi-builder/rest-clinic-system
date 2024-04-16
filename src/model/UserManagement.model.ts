75ee14d652df958d070760d9d57fe0880eead7d8
azkifairuz <azkiajmal@gmail.com>
2024-04-16 22:27:25 +0700

    get all user

--- src/model/UserManagement.model.ts
+++ src/model/UserManagement.model.ts
@@ -5,24 +5,9 @@ export class RegisterUserRequest {
   role: string;
 }
 
-export class PaginationData<T> {
-  data: T[];
-  page: number;
-  size: number;
-  total_page: number;
-  total_data: number;
-}
-
 export class UserResponse {
   uuid: string;
   username: string;
   fullname: string;
   role: string;
-}
-
-export class BaseResponse<T> {
-  data?: T;
-  pagination?: PaginationData<T>;
-  status_code: number;
-  message: string;
 }
--- src/user-management/user-management.service.ts
+++ src/user-management/user-management.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { PrismaService } from 'src/common/prisma.service';
+import { BaseResponse, PaginationData } from 'src/model/BaseResponse.model';
+import { UserResponse } from 'src/model/UserManagement.model';
 
 @Injectable()
-export class UserManagementService {}
+export class UserManagementService {
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private logger: Logger,
+    private prismaService: PrismaService,
+  ) {}
+
+  async getAllUsers(
+    page: number,
+    size: number,
+  ): Promise<BaseResponse<UserResponse[]>> {
+    const totalCount = await this.prismaService.user.count();
+    const totalPages = Math.ceil(totalCount / size);
+
+    const users = await this.prismaService.user.findMany({
+      skip: (page - 1) * size,
+      take: size,
+      select: {
+        uuid: true,
+        username: true,
+        full_name: true,
+        roles: true,
+      },
+    });
+
+    const userResponses: UserResponse[] = users.map((user) => ({
+      uuid: user.uuid,
+      username: user.username,
+      fullname: user.full_name,
+      role: user.roles,
+    }));
+
+    const paginationData: PaginationData = {
+      page,
+      size,
+      total_page: totalPages,
+      total_data: totalCount,
+    };
+
+    return {
+      data: userResponses,
+      pagination: paginationData,
+      status_code: 200,
+      message: 'success',
+    };
+  }
+}
