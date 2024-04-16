eedc99b770afe55c533989d6ac3c9c9d756b0430
azkifairuz <azkiajmal@gmail.com>
2024-04-17 00:11:49 +0700

    delete  user

--- src/user-management/user-management.controller.ts
+++ src/user-management/user-management.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, HttpCode, Query, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Query,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UserResponse } from 'src/model/UserManagement.model';
 import { BaseResponse } from 'src/model/BaseResponse.model';
 import { UserManagementService } from './user-management.service';
@@ -22,6 +29,15 @@ export class UserManagementController {
     @Param() params: { uuid: string },
   ): Promise<BaseResponse<UserResponse>> {
     const user = await this.userService.getDetailUser(params.uuid);
+    return user;
+  }
+
+  @Delete(':uuid')
+  @HttpCode(200)
+  async delete(
+    @Param() params: { uuid: string },
+  ): Promise<BaseResponse<string>> {
+    const user = await this.userService.deleteUser(params.uuid);
     return user;
   }
 }
--- src/user-management/user-management.service.ts
+++ src/user-management/user-management.service.ts
@@ -48,17 +48,23 @@ export class UserManagementService {
   }
 
   async getDetailUser(uuid: string): Promise<BaseResponse<UserResponse>> {
-    const users = await this.prismaService.user.findUnique({
+    const user = await this.prismaService.user.findUnique({
       where: {
         uuid: uuid,
       },
     });
-
+    if (!user) {
+      return {
+        data: null,
+        status_code: 404,
+        message: 'user tidak ditemukan',
+      };
+    }
     const userResponses: UserResponse = {
-      uuid: users.uuid,
-      username: users.username,
-      role: users.roles,
-      fullname: users.full_name,
+      uuid: user.uuid,
+      username: user.username,
+      role: user.roles,
+      fullname: user.full_name,
     };
 
     return {
@@ -66,5 +72,35 @@ export class UserManagementService {
       status_code: 200,
       message: 'success',
     };
+  }
+
+  async deleteUser(uuid: string): Promise<BaseResponse<string>> {
+    try {
+      const deleteUser = await this.prismaService.user.delete({
+        where: {
+          uuid: uuid,
+        },
+      });
+
+      if (!deleteUser) {
+        return {
+          data: null,
+          status_code: 400,
+          message: 'Gagal menghapus user',
+        };
+      }
+
+      return {
+        data: null,
+        status_code: 200,
+        message: `Berhasil menghapus ${deleteUser.full_name}`,
+      };
+    } catch (error) {
+      return {
+        data: null,
+        status_code: 500,
+        message: `Gagal menghapus user: ${error.message}`,
+      };
+    }
   }
 }
