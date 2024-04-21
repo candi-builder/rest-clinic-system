96f6090ff3122db6f653ee3d656ffe9c73b0d5fd
azkifairuz <azkiajmal@gmail.com>
2024-04-21 20:08:54 +0700

    update antrian status

--- src/antrian/antrian.controller.ts
+++ src/antrian/antrian.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { BaseResponse } from 'src/model/BaseResponse.model';
 import { AntrianResponse } from 'src/model/antrian.model';
 import { AntrianService } from './antrian.service';
+import { Status } from '@prisma/client';
 
 @Controller('antrian')
 export class AntrianController {
@@ -18,6 +19,23 @@ export class AntrianController {
       size,
       dokterId,
     );
+    return antrian;
+  }
+  @Get('/pickup')
+  async getAntrianPickup(
+    @Query('page') page: number = 1,
+    @Query('size') size: number = 10,
+  ): Promise<BaseResponse<AntrianResponse[]>> {
+    const antrian = await this.antrianService.getAntrianPickup(page, size);
+    return antrian;
+  }
+
+  @Post(':antrianId')
+  async post(
+    @Param('antrianId') antrianId: number,
+    @Body('status') status: Status
+  ): Promise<BaseResponse<string>> {
+    const antrian = await this.antrianService.updateStatus(antrianId,status);
     return antrian;
   }
 }
--- src/antrian/antrian.service.ts
+++ src/antrian/antrian.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Status } from '@prisma/client';
 import { PrismaService } from 'src/common/prisma.service';
 import { BaseResponse, PaginationData } from 'src/model/BaseResponse.model';
 import { AntrianResponse } from 'src/model/antrian.model';
+import { Utils } from 'src/utils/utils';
 
 @Injectable()
 export class AntrianService {
@@ -10,17 +12,41 @@ export class AntrianService {
   async getAntrianDocter(
     page: number,
     size: number = 10,
-    doctor: string
+    doctor: string,
   ): Promise<BaseResponse<AntrianResponse[]>> {
-    const totalCount = await this.prismaService.antrian.count();
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const totalCount = await this.prismaService.antrian.count({
+      where: {
+        poli: {
+          user_id: doctor,
+        },
+        tanggal: {
+          gte: today,
+          lt: new Date(today.getTime() + 24 * 60 * 60 * 1000),
+        },
+        OR: [
+          {
+            status: 'WAITING',
+          },
+          {
+            status: 'CHECKING',
+          },
+        ],
+      },
+    });
     const totalPages = Math.ceil(totalCount / size);
     const data = await this.prismaService.antrian.findMany({
       skip: (page - 1) * size,
       take: size,
-      where:{
-        poli:{
-          user_id:doctor
-        }
+      where: {
+        poli: {
+          user_id: doctor,
+        },
+        tanggal: {
+          gte: today,
+          lt: new Date(today.getTime() + 24 * 60 * 60 * 1000),
+        },
       },
       select: {
         passien: {
@@ -47,6 +73,7 @@ export class AntrianService {
         tanggal: true,
       },
     });
+
     const paginationData: PaginationData = {
       page,
       size,
@@ -54,18 +81,135 @@ export class AntrianService {
       total_data: totalCount,
     };
     const AntrianResponse: AntrianResponse[] = data.map((antrian) => ({
-      nomor_bpjs:antrian.passien.nomor_bpjs,
-      nama_passien:antrian.passien.nama_passien,
+      nomor_bpjs: antrian.passien.nomor_bpjs,
+      nama_passien: antrian.passien.nama_passien,
       dokter: antrian.poli.user.username,
-      poli:antrian.poli.poli.poli_name,
-      status:antrian.status
+      poli: antrian.poli.poli.poli_name,
+      status: antrian.status,
     }));
 
     return {
       data: AntrianResponse,
       pagination: paginationData,
-      status_code:200,
+      status_code: 200,
       message: 'success',
     };
+  }
+
+  async getAntrianPickup(
+    page: number,
+    size: number = 10,
+  ): Promise<BaseResponse<AntrianResponse[]>> {
+    const totalCount = await this.prismaService.antrian.count(
+
+    );
+    const totalPages = Math.ceil(totalCount / size);
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const data = await this.prismaService.resep.findMany({
+      skip: (page - 1) * size,
+      take: size,
+      select:{
+        hasil_diagnosa:true,
+        keterangan_resep:true,
+        tanggal_resep:true,
+        pasien:{
+          select:{
+            nomor_bpjs:true,
+            nama_passien:true,
+            Antrian:{
+              where: {
+                tanggal: {
+                  gte: today,
+                  lt: new Date(today.getTime() + 24 * 60 * 60 * 1000),
+                },
+                status: "PICKUP"
+              },
+              select: {
+                poli: {
+                  select: {
+                    user: {
+                      select: {
+                        username: true,
+                      },
+                    },
+                    poli: {
+                      select: {
+                        poli_name: true,
+                      },
+                    },
+                  },
+                },
+                status: true,
+                tanggal: true,
+              },
+            }
+          }
+        }
+      }
+    });
+    const paginationData: PaginationData = {
+      page,
+      size,
+      total_page: totalPages,
+      total_data: totalCount,
+    };
+    const antrianResponses: AntrianResponse[] = [];
+
+    data.forEach((resep) => {
+        resep.pasien.Antrian.forEach((antrian) => {
+            const response: AntrianResponse = {
+                nomor_bpjs: resep.pasien.nomor_bpjs,
+                nama_passien: resep.pasien.nama_passien,
+                dokter: antrian.poli.user.username,
+                poli: antrian.poli.poli.poli_name,
+                status: antrian.status,
+                hasil_diagnosa: resep.hasil_diagnosa,
+                keterangan_resep: resep.keterangan_resep,
+            };
+    
+            antrianResponses.push(response);
+        });
+    });
+    
+    return {
+      data: antrianResponses,
+      pagination: paginationData,
+      status_code: 200,
+      message: 'success',
+    };
+  }
+
+  async updateStatus(
+    antrianId:number,
+    status:Status,
+  ):Promise<BaseResponse<string>>{
+    const antrianToUpdate = await this.prismaService.antrian.findUnique(
+      {
+        where:{
+          id:Number(antrianId)
+        }
+      }
+    )
+    if (!antrianToUpdate) {
+      return{
+        status_code:404,
+        message:"antrian tidak ditemukan"
+      }
+    }
+    await this.prismaService.antrian.update(
+      {
+        where:{
+          id:Number(antrianId)
+        },
+        data:{
+          status:status
+        }
+      }
+    )
+    return {
+      status_code:201,
+      message:`Beharhasil Mengubah status ke ${status}`
+    }
   }
 }
