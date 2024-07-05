4be241c089ae63f2ecdd3f09088efef5fdd1bb58
basstimam <basstimam@gmail.com>
2024-07-05 18:00:23 +0700

    fix: nullable on kelas rawat

--- src/passien/passien/passien.validation.ts
+++ src/passien/passien/passien.validation.ts
@@ -10,7 +10,7 @@ export class PassienValidation{
         nama_passien: z.string().min(3).max(255),
         tanggal_lahir: z.string(),
         alamat: z.string().min(3).max(255),
-        kelas_rawat: z.string().min(3).max(255).optional(),
+        kelas_rawat: z.string().optional(),
         faskes_tingkat_satu: z.string().min(3).max(255),
         poli_id: z.number(),
     })
